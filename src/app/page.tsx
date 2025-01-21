bbc1e37ba5fb65938c7b24e636b4ed91d6372d49
hasnat.jamil <hasnat.jamil@enosisbd.com>
2025-01-22 02:06:47 +0600

    add oauth buttons, card container, card header for the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,14 @@
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
-      <Card className="w-[300px]">
-        <CardHeader>
-          <CardTitle>Sign In</CardTitle>
-          <CardDescription>Sign in to your account</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Button>Sign In</Button>
-        </CardContent>
-      </Card>
+      <Button>
+        <Link href="/login">Login</Link>
+      </Button>
     </>
   );
 };
